d886117bb1c2d62c9a320be648b1987101a1d43a
nicolifelix <nicolifelix@hotmail.com>
2021-09-10 15:59:22 -0300

    Ruby Challenge :sparkles:

--- Ruby/Métodos/Ruby_Methods-Procs.rb
+++ Ruby/Métodos/Ruby_Methods-Procs.rb
@@ -1,14 +1,14 @@
-A passagem de blocos é uma maneira de passar funções como argumentos para outras funções.
+# A passagem de blocos é uma maneira de passar funções como argumentos para outras funções.
 
-Os blocos são uma das poucas exceções à regra "tudo é um objeto" em Ruby. Os blocos não são objetos e não podem ser salvos em variáveis.
+# Os blocos são uma das poucas exceções à regra "tudo é um objeto" em Ruby. Os blocos não são objetos e não podem ser salvos em variáveis.
 
-Os objetos Proc são blocos de código que podem ser associados a um conjunto de variáveis ​​locais. Você pode pensar em umobjeto proc como um bloco "salvo".
+# Os objetos Proc são blocos de código que podem ser associados a um conjunto de variáveis ​​locais. Você pode pensar em umobjeto proc como um bloco "salvo".
 
-Procs são uma ótima maneira de manter seu código DRY . DRY significa Do Not Repeat Yourself .
+# Procs são uma ótima maneira de manter seu código DRY . DRY significa Do Not Repeat Yourself .
 
-Exemplo:
+# Exemplo:
 
-CÓDIGO
+# CÓDIGO
 
 def foo(a, b, my_proc)
     my_proc.call(a, b)
@@ -22,17 +22,17 @@ SAÍDA
 
 25
 
-Neste exemplo, criamos o proc add , que adiciona dois números.
-O proc add é passado como um parâmetro para o método foo .
-No método foo , my_proc.call(a, b)executa o proc.
+# Neste exemplo, criamos o proc add , que adiciona dois números.
+# O proc add é passado como um parâmetro para o método foo .
+# No método foo , my_proc.call(a, b)executa o proc.
 
-Tarefa
+# Tarefa
 
-Você recebe um código parcialmente completo. Sua tarefa é preencher os espaços em branco ( ______).
+# Você recebe um código parcialmente completo. Sua tarefa é preencher os espaços em branco ( ______).
 
-O método square_of_sum calcula a soma dos elementos em uma matriz de entrada e retorna o quadrado dos elementos somados.
+# O método square_of_sum calcula a soma dos elementos em uma matriz de entrada e retorna o quadrado dos elementos somados.
 
-Por exemplo :
+# Por exemplo :
 
 Input array: [1, 2, 3]
 Output: 36
